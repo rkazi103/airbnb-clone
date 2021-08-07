8b59662e7093eafb016593c307c99eb8309e1da8
Rayan Kazi <rayan.quack.you@gmail.com>
2021-08-07 14:50:14 -0700

    implemented server side rendering

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Banner from "@components/Banner";
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div>
       <Head>
@@ -12,6 +12,28 @@ export default function Home() {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {exploreData.map((item) => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const exploreData = await fetch("https://jsonkeeper.com/b/4G1G").then((res) =>
+    res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
+};
