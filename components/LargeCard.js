887537f8d02b73956b1ee78de9fb0a0ae1b08d7f
Rayan Kazi <rayan.quack.you@gmail.com>
2021-08-08 12:57:18 -0700

    built the large card

---
+++ components/LargeCard.js
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import Image from "next/image";
+
+const LargeCard = ({ img, title, description, buttonText }) => {
+  return (
+    <section className="relative py-16 cursor-pointer">
+      <div className="relative h-96 min-w-[300px]">
+        <Image
+          src={img}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-2xl"
+        />
+      </div>
+
+      <div className="absolute top-32 left-12">
+        <h3 className="text-4xl mb-3 w-64">{title}</h3>
+        <p>{description}</p>
+
+        <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5 transition duration-150 hover:bg-white hover:text-gray-900">
+          {buttonText}
+        </button>
+      </div>
+    </section>
+  );
+};
+
+LargeCard.propTypes = {
+  img: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  buttonText: PropTypes.string,
+};
+
+export default LargeCard;
