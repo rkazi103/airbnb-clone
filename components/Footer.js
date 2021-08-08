dc01083c73294764955acb5c8236c3477fb2be40
Rayan Kazi <rayan.quack.you@gmail.com>
2021-08-08 13:06:35 -0700

    built the footer

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+const Footer = () => {
+  return (
+    <footer className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb Works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This isn't a real site</p>
+        <p>Amazing Clone</p>
+        <p>Referrals Accepted</p>
+        <p>Let's Go</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Wumbo</p>
+        <p>Presents</p>
+        <p>The Airbnb Clone</p>
+        <p>Github Has</p>
+        <p>More Projects</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Support</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi</p>
+        <p>Easter Eggs</p>
+        <p>For the Win</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
