8987a3b77cd90329a2c12cee5c2aac642b1aa3da
Rayan Kazi <rayan.quack.you@gmail.com>
2021-08-14 19:38:22 -0700

    built the info card

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,4 +1,7 @@
 import PropTypes from "prop-types";
+import Image from "next/image";
+import { HeartIcon } from "@heroicons/react/outline";
+import { StarIcon } from "@heroicons/react/solid";
 
 const InfoCard = ({
   img,
@@ -11,7 +14,37 @@ const InfoCard = ({
   long,
   lat,
 }) => {
-  return <div></div>;
+  return (
+    <div className="flex py-7 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t">
+      <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink">
+        <Image src={img} layout="fill" objectFit="cover" />
+      </div>
+
+      <div className="flex flex-col flex-grow pl-5">
+        <div className="flex justify-between">
+          <p>{location}</p>
+          <HeartIcon className="h-7 cursor-pointer" />
+        </div>
+
+        <h4 className="text-xl">{title}</h4>
+        <div className="border-b w-10 pt-2" />
+
+        <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
+
+        <div className="flex justify-between items-end pt-5">
+          <p className="flex items-center">
+            <StarIcon className="h-5 text-red-400" />
+            {star}
+          </p>
+
+          <div>
+            <p className="text-lg font-semibold pb-2 lg:text-2xl">{price}</p>
+            <p className="text-right font-extralight">{total}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 InfoCard.propTypes = {
--- pages/search.js
+++ pages/search.js
@@ -50,6 +50,7 @@ const Search = ({ searchResults }) => {
                 lat,
               }) => (
                 <InfoCard
+                  key={img}
                   img={img}
                   location={location}
                   title={title}
